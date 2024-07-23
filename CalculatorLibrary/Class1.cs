1081dc43807cdd3da7c3a93a67967bfd50c63daa
deoresulakshana <deoresulakshana@gmail.com>
2024-07-23 10:08:11 +0530

    Add project files.

---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,113 @@
+﻿namespace CalculatorLibrary
+{
+    public class Calculator : ISimpleCalculator
+    {
+        public int AddNos(int fno, int sno)
+        {
+           return fno + sno;    
+        }
+
+        public int DivideNos(int fno, int sno)
+        {
+            return fno/ sno;
+        }
+
+        public int MultiplyNos(int fno, int sno)
+        {
+            return fno* sno;
+        }
+
+        public int SubtractNos(int fno, int sno)
+        {
+            return (fno - sno);
+        }
+    }
+
+
+    public class ScientificCalculator : Calculator, IScientificCalculator
+    {
+        public double Cos(double v)
+        {
+         double cosValue= Math.Cos(v);
+            return  cosValue;   
+        }
+
+        public double Sin(double v)
+        {
+          double sinValue=Math.Sin(v);  
+            return sinValue;
+        }
+
+        public double Tan(double v)
+        {
+            return Math.Tan(v);
+        }
+    }
+
+
+
+    public class SpecialCalci : IScientificCalculator, ISpecial
+    {
+        public double Cos(double v)
+        {
+            //throw new NotImplementedException();
+            return Math.Cos(v);
+        }
+
+        public double Sin(double v)
+        {
+            //throw new NotImplementedException();
+            return Math.Sin(v);
+        }
+
+
+
+        public double Square(double fno)
+        {
+            // throw new NotImplementedException();
+            return fno * fno;
+        }
+
+        public double SquareRoot(double v)
+        {
+            return Math.Sqrt(v);
+        }
+
+        public double Tan(double v)
+        {
+            return Math.Tan(v);
+        }
+    }
+
+
+
+    public interface ISimpleCalculator
+    {
+        public int AddNos(int fno, int sno);
+        public int SubtractNos(int fno, int sno);
+        public int MultiplyNos(int fno, int sno);
+        public int DivideNos(int fno, int sno);
+
+    
+    }
+
+    public interface IScientificCalculator
+    {
+
+        public double Sin(double v);
+        public double Cos(double v);
+        public double Tan(double v);
+
+
+    }
+
+    public interface ISpecial
+    { 
+    public double SquareRoot(double v);
+    public double Square(double fno);
+          
+    
+    }
+
+
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using CalculatorLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Menu");
+        Console.WriteLine("1 for simple, 2 for scientific,3 for special");
+        int c=Convert.ToInt32(Console.ReadLine());
+        switch (c)
+        {
+            case 1:
+                Console.WriteLine("1. add 2. subtract 3. multiply 4.divide");
+                //Calculator  object
+
+                break;
+            case 2:
+                Console.WriteLine("1. add 2. subtract 3. multiply 4.divide 5. Sin 6. cos 7. Tan");
+                //ScientificCalculator obj
+                break;
+                case 3:
+                Console.WriteLine("1. Sin 2. cos 3. Tan 4. Square 5.SquareRoot");
+                //SpecialCalci
+                break;
+            
+        }
+    }
+}
